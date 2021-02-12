858c66f30cc81bef1ec12930a63f01c283a0f48e
yasser1dev <yassirchihab1@gmail.com>
2021-02-12 16:50:40 +0100

    kafka producer

--- src/main/java/com/example/mss/KafkaProducerMssApplication.java
+++ src/main/java/com/example/mss/KafkaProducerMssApplication.java
@@ -1,9 +1,15 @@
 package com.example.mss;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Collection;
 
 @SpringBootApplication
 @EnableKafka
@@ -13,5 +19,16 @@ public class KafkaProducerMssApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaProducerMssApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(RestOperationClient restOperationClient,KafkaTemplate<String,Operation> kafkaTemplate){
+        final String topic="operation";
+        return args -> {
+            PagedModel<Operation> operations=restOperationClient.getAllOperations();
+            operations.forEach((operation -> {
+                System.out.println(operation);
+                System.out.println("#########################");
+                kafkaTemplate.send(topic,"key : "+operation.getNumero(),operation);
+            }));
+        };
+    }
 }
--- src/main/java/com/example/mss/RestOperationClient.java
+++ src/main/java/com/example/mss/RestOperationClient.java
@@ -4,14 +4,16 @@ package com.example.mss;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.ws.rs.core.Context;
 import java.util.Collection;
 
 @FeignClient(name = "COMPTES-SERVICE")
 public interface RestOperationClient {
-    @GetMapping(path = "/operations")
-    Collection<Operation> getAllOperations();
+    @GetMapping(path = "/operations/")
+    PagedModel<Operation> getAllOperations();
 }
